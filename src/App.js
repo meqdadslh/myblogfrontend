3b7971c0a8ff25df3b961d99bb7c62a43a311950
Meqdad Ali <meqdadali@Meqdads-MacBook-Pro.local>
2021-06-21 15:29:30 -0400

    created components and passed props to the components and created the edit and delete buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState, useEffect} from 'react';
+import ArticleList from './components/ArticleList';
+import Form from './components/Form';
+
 
 function App() {
+
+  const [articles, setArtciles] = useState ([])
+  const [editArticle, setEditArtcile] = useState ()
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/blog/articles/', {
+      'method': 'GET',
+      headers : {
+        'content-Type':'application/json',
+        'Authorization':'Token 79b95aeebc9dc0494727ed8ab01bc14636e3fadf'
+      }
+    })
+    .then(resp => resp.json())
+    // set the articles to the response that we have 
+    .then(resp => setArtciles(resp))
+    // if there is an error I want to catch it
+    .catch(error => console.log(error))
+  }, [])
+
+
+  const editBtn = (article) => {
+    setEditArtcile(article)
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ArticleList articles = {articles}/>
+      <Form/>
     </div>
   );
 }
---
+++ src/components/ArticleList.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function ArticleList(props) {
+
+    const editBtn = (article) => {
+        props.editBtn(article)
+    }
+    return (
+        <div>
+        {props.articles.map(article => {
+        return (
+          <div key={article.id}>
+        <h2>{article.title}</h2>
+        <p>{article.description}</p>
+        
+        <div className = "row">
+            <div className = "col-md-1">
+                <button className = "btn btn-primary"   onClick = {() => 
+                    editBtn(article)}>
+                        Update
+                </button>
+            </div>
+            <div className = "col">
+                <button className = "btn btn-danger">Delete</button>
+            </div>
+
+        </div>
+        <hr/>
+        </div>
+        
+        )
+      })}
+        </div>
+    )
+}
+
+export default ArticleList
